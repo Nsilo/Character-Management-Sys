644045c85ab49a1d5bf75f120981e0a1c5e81fda
Jivaka <jivakapyane93@gmail.com>
2021-02-27 12:35:59 -0800

    first commit and layout created

---
+++ db/index.js
@@ -0,0 +1,57 @@
+const connection = require("./connection");
+require("console.table");
+
+class DB {
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  findAllEmployees() {
+    return this.connection.query(
+      "SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS Employee, role.title, role.salary, department.deptname AS department FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id;"
+    );
+  }
+
+  findAllRoles() {
+    return this.connection.query(
+      "SELECT role.id, title, deptname FROM department JOIN role ON department.id = department_id;"
+    );
+  }
+
+  findAllDepts() {
+    return this.connection.query("SELECT * FROM department");
+  }
+
+  createEmployee(employee) {
+    return this.connection.query("INSERT INTO employee SET ?", employee);
+  }
+
+  createRole(role) {
+    return this.connection.query("INSERT INTO role SET ?", role);
+  }
+
+  createDept(department) {
+    return this.connection.query("INSERT INTO department SET ?", department);
+  }
+
+  updateEmployeeRole(roleId, employeeId) {
+    return this.connection.query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
+    );
+  }
+
+  deleteEmployee(employee) {
+    return this.connection.query("DELETE FROM employee WHERE id = ?;", [
+      employee,
+    ]);
+  }
+
+  findEmpDepts() {
+    return this.connection.query(
+      "SELECT CONCAT(first_name, ' ', last_name) AS Employee, deptname as DeptName FROM department JOIN role ON department.id = department_id JOIN employee ON role.id = role_id ORDER BY DeptName DESC;"
+    );
+  }
+}
+
+module.exports = new DB(connection);
---
+++ db/seeds.sql
@@ -0,0 +1,32 @@
+USE employeeTrackerDB;
+
+INSERT INTO department (deptname)
+VALUES ("Sales"), ("Legal"), ("Finance"), ("Engineering"); 
+
+INSERT INTO role (title, salary, department_id) VALUES ("Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Salesman", 80000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Sr Counsel", 110000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 125000, 3); 
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Dev", 150000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sasuke", "Uchiha", 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Naruto", "Uzumaki", 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Eren", "Yaeger", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Light", "Yagami", 3, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Mikasa", "Ackerman", 2, 5);
+
+SELECT DISTINCT department_id AS id, CONCAT(first_name, ' ', last_name) AS Employee, deptname as DeptName, title, salary
+FROM department
+JOIN role ON department.id = department_id
+JOIN employee ON role.id = role_id;
+
+SELECT department_id, deptname, title, salary FROM department JOIN role ON department.id = department_id;
+
+SELECT department_id as id, CONCAT(first_name, ' ', last_name) AS Employee, deptname as DeptName FROM department JOIN role ON department.id = department_id JOIN employee ON role.id = role_id ORDER BY DeptName DESC;
+;
+
+SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS Employee, role.title, department.deptname AS department, role.salary FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;
+
+DELETE FROM employee WHERE employee.id = 2;
+
+SELECT * FROM employee;
